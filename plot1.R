ab82b3d9b419034cd37d96a522702d5ce979f9cd
tushar ​ montaño <tmontano@gmail.com>
2015-02-04 15:54:39 -0700

    plot1.R
    works with comments

--- plot1.R
+++ plot1.R
@@ -1,16 +1,35 @@
+## author:  tushar montaño
+## date:    2015.02.04
+## Exploratory Data Analysis
+## Course Project 1
+## JHU Data Sciece Specialization
+
 ## Getting full dataset
+## assumes that the dataset is one directory above and down from
+## the current working directory
+
 data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?",
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+## format the $Date column as "dd/mm/yyy" using as.date
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-## Subsetting the data
+
+## Subset the data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## release memory from the full data set
 rm(data_full)
+
 ## Converting dates
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
+
 ## Plot 1
 hist(data$Global_active_power, main="Global Active Power",
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
 ## Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
+
+## end
